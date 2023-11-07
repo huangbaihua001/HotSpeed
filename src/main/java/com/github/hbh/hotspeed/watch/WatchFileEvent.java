33a90665371759fbc41a00d10beb044584491140
huangbaihua <huangbaihua@vip.qq.com>
2023-11-07 10:07:11 +0800

    feat(watcher): copy watcher implementation from Hotswap Agent.

--- src/main/java/com/github/hbh/hotspeed/watch/WatchFileEvent.java
+++ src/main/java/com/github/hbh/hotspeed/watch/WatchFileEvent.java
@@ -1,13 +1,50 @@
+/*
+ * Copyright 2013-2023 the HotswapAgent authors.
+ *
+ * This file is part of HotswapAgent.
+ *
+ * HotswapAgent is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * HotswapAgent is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with HotswapAgent. If not, see http://www.gnu.org/licenses/.
+ */
 package com.github.hbh.hotspeed.watch;
 
 import java.net.URI;
 
+/**
+ * An event on filesystem.
+ *
+ * @author Jiri Bubnik
+ */
 public interface WatchFileEvent {
-  FileEvent getEventType();
+  /**
+   * @return type of the event
+   */
+  public FileEvent getEventType();
 
-  URI getURI();
+  /**
+   * URI to file or directory with the event
+   *
+   * @return URI
+   */
+  public URI getURI();
 
-  boolean isFile();
+  /**
+   * URI is a file.
+   */
+  public boolean isFile();
 
-  boolean isDirectory();
+  /**
+   * URI is a directory.
+   */
+  public boolean isDirectory();
 }
